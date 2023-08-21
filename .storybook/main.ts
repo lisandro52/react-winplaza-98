fc36f070570e2157b9a3d0471a90589ea146a9c1
Lisandro Cobeaga <lisandro.cobeaga@inworx.com>
2023-08-21 20:47:12 -0300

    Added checkbox, fixed rollup config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,6 +6,15 @@ const config: StorybookConfig = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-styling',
+      options: {
+        sass: {
+          // Require your Sass preprocessor here
+          implementation: require('sass'),
+        },
+      },
+    },
   ],
   framework: {
     name: '@storybook/react-webpack5',
